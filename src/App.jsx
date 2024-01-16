2292f597de0dd536e1aee00a44e265b6c78db7df
Aakanksha <82.25aakanksha@gmail.com>
2024-01-17 01:12:02 +0530

    json updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,25 @@ import Post from './Post';
 import NotFound from './NotFound';
 
 const App = () => {
+  const [clientData, setClientData] = React.useState('');
+  const port = import.meta.env.VITE_SECONDARY_PUBLIC_PORT
+  const playgroundHostname = new URL (`https://${import.meta.env.VITE_PUBLIC_HOSTNAME}`)
+  const dataURL = `https://${playgroundHostname.hostname.split('.')[0]}-${port}.codedamn.app`
+  
+  React.useEffect(() => {
+    fetch(`${dataURL}/blogs`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => setClientData(data))
+      .catch(error => {
+        // Handle the error here, for example, log it or update state with an error message.
+        console.error('Error fetching data:', error);
+      });
+  }, []);
   return (
    
       <div className="App">
