16c4ae9b0fb7b9e812ac29636955e3c6c20123c6
Aakanksha <82.25aakanksha@gmail.com>
2024-01-17 23:15:19 +0530

    changed .envfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
@@ -8,12 +9,10 @@ import Post from './Post';
 import NotFound from './NotFound';
 
 const App = () => {
-  const [clientData, setClientData] = React.useState('');
-  const port = import.meta.env.VITE_SECONDARY_PUBLIC_PORT
-  const playgroundHostname = new URL (`https://${import.meta.env.VITE_PUBLIC_HOSTNAME}`)
-  const dataURL = `https://${playgroundHostname.hostname.split('.')[0]}-${port}.codedamn.app`
+  const [clientData, setClientData] = useState('');
+  const hostnames = import.meta.env.VITE_SECONDARY_PUBLIC_HOSTNAME
   
-  React.useEffect(() => {
+ useEffect(() => {
     fetch(`${dataURL}/blogs`)
       .then(response => {
         if (!response.ok) {
@@ -23,7 +22,6 @@ const App = () => {
       })
       .then(data => setClientData(data))
       .catch(error => {
-        // Handle the error here, for example, log it or update state with an error message.
         console.error('Error fetching data:', error);
       });
   }, []);
@@ -31,8 +29,9 @@ const App = () => {
    
       <div className="App">
         <Navbar />
-        <Outlet/>
-      
+      {  
+        //  <Outlet/>
+      }
       </div>
    
   );
